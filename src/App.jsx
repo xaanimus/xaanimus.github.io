ce386c76835a3326adca0c699d9adb49d0a0aae5
Serge-Olivier Amega <sergeolivieramega@gmail.com>
2018-07-22 11:28:59 -0700

    re init

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+//@flow
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import React, { Component } from 'react';
 import './App.css';
 import AppData from './AppData';
@@ -19,17 +20,21 @@ const redirectToAppStoreComponent = () => {
   return <div></div>;
 };
 
+function mkPath(path: string): string {
+  return path;
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <div>
-            <Route exact path="/" component={Main}/>
-            <Route exact path="/project/:projectid" component={ProjectDetail}/>
-            <Route exact path="/apps.html" component={redirectToAppStoreComponent}/>
-          </div>
-        </Router>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route exact path={mkPath("/")} component={Main}/>
+            <Route exact path={mkPath("/project/:projectid")} component={ProjectDetail}/>
+            <Route exact path={mkPath("/apps.html")} component={redirectToAppStoreComponent}/>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
--- src/PortfolioData.js
+++ src/PortfolioData.js
@@ -3,31 +3,10 @@ import expcalc_gif from './assets/expcalc.gif';
 import currency_gif from './assets/currency.gif';
 import dodgeblocks_gif from './assets/dodgeblocks.gif';
 
-import Descriptions from './PortfolioDescriptions';
+import projects from './PortfolioProjects';
 
 const PortfolioData = {
-  projects: {
-    snakeDuo: {
-      title: "SnakeDuo",
-      imgSrc: snkd_gif,
-      description: Descriptions.snakeDuo
-    },
-    exposureCalc: {
-      title: "Exposure Calc",
-      imgSrc: expcalc_gif,
-      description: Descriptions.exposureCalc
-    },
-    dodgeBlocks: {
-      title: "Dodge Blocks!",
-      imgSrc: dodgeblocks_gif,
-      description: Descriptions.dodgeBlocks
-    },
-    currencyConvert: {
-      title: "Currency Convert Live",
-      imgSrc: currency_gif,
-      description: Descriptions.currencyConvert
-    }
-  }  
+  projects: projects
 };
 
 export default PortfolioData;
