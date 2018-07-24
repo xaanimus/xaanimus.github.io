5b949f7d2ab69b9dfedc24c6ddeea40f7ed6c8a5
Serge-Olivier Amega <sergeolivieramega@gmail.com>
2018-07-23 22:27:43 -0700

    modify data

--- src/PortfolioData.js
+++ src/PortfolioData.js
@@ -5,8 +5,84 @@ import dodgeblocks_gif from './assets/dodgeblocks.gif';
 
 import projects from './PortfolioProjects';
 
+const FullPortfolioData = {
+    projects: projects,
+    education: [
+        {
+            institutionName: "Cornell University",
+            time: "2015-2019 (Expected)",
+            content: "Computer Science\nIthaca, NY"
+        }
+    ],
+    honors: [
+        "McMullen Dean's Scholar",
+        "Dean's List"
+    ],
+    courses: [
+        "Honors Object Oriented Design and Data Structures",
+        "Operating Systems",
+        "Algorithms",
+        "Graphics",
+        "Discrete Structures",
+        "Functional Programming",
+        "Computer Systems Organization",
+        "Database Systems",
+        "Engineering Probability and Statistics",
+        "Linear Algebra",
+        "Multivariable Calculus"
+    ],
+    programmingLangs: {
+        proficient: [
+            "Swift",
+            "Objective-C",
+            "Objective-C++",
+            "C++",
+            "C",
+            "Java",
+            "Javascript (Node.js)",
+            "OCaml",
+            "Python",
+            "Golang",
+            "Rust",
+            "GLSL"
+        ]
+    },
+    technologies: [
+        "MySQL",
+        "Git",
+        "Mercurial",
+        "GraphQL",
+        "REST",
+        "OpenGL",
+        "WebGL",
+        "iOS/Cocoa Touch",
+        "Unity",
+        "Qt",
+        "Shell"
+    ],
+    skills: [
+        "Object-Oriented Programming",
+        "Functional Programming",
+        "GUI Programming",
+        "Graphics Programming",
+        "Code Profiling and Optimization",
+        "Algorithms & Data Structures"
+    ]
+};
+
+const projectsToDisplay = [
+    "snakeDuo",
+    "exposureCalc",
+    "dodgeBlocks",
+    "currencyConvert",
+].reduce((acc, key) => {
+    acc[key] = FullPortfolioData.projects[key];
+    return acc;
+}, {});
+
+
 const PortfolioData = {
-  projects: projects
+    projects: projectsToDisplay
 };
 
 export default PortfolioData;
--- src/PortfolioProjects.js
+++ src/PortfolioProjects.js
@@ -38,4 +38,52 @@ projects.currencyConvert.description = `
 A simple currency converter that converts between 30 popular currencies. I used the Fixer.io API to get exchange rates, and the SwiftyJSON library.
 `;
 
+projects.xsray = {
+    title: "Xsray - Path Tracer built with rust",
+    description: "A path tracer built with rustlang",
+    link: "https://github.com/xaanimus/xsray"
+};
+
+projects.fb2017 = {
+    title: "Facebook",
+    subtitle: "Software Engineering Intern - iOS",
+    time: "Summer 2017",
+    bullet: [
+        "Utilized A/B testing to maximize effectivness of features",
+        "Contributed to the development of features on the Facebook app using Component-based architecture and GraphQL",
+        "Collaborated with team of developers, product managers and designers to improve the functionality of the app"
+    ]
+};
+
+projects.independentIosDev = {
+    title: "Independent iOS Developer",
+    time: "2014 - 2017",
+    bullet: [
+        "Built and submitted iOS apps to the iTunes App Store",
+        "Developed server backends for iOS apps using Node.js and Golang",
+        "Used the MVC design pattern to build apps with User Interfaces in an organized and maintainable way",
+        "Experience with client/server communication using REST and Websockets",
+    ]
+};
+
+projects.creativecomputing = {
+    title: "Creative Computing Club",
+    subtitle: "Co-founder, Project Software Lead",
+    time: "Jan 2017 - Now",
+    bullet: [
+        "Lead software teams for projects",
+        "Collaborated with other founders to build a budget and organize club structure"
+    ]
+};
+
+projects.appdev = {
+    title: "Cornell AppDev",
+    subtitle: "Software Developer, Project Lead",
+    time: "Fall 2017 - Now",
+    bullet: [
+        "Project lead for CU Here in Spring 2017",
+        "Used ARKit and SceneKit to create an AR app to explore campus",
+    ]
+};
+
 export default projects;
